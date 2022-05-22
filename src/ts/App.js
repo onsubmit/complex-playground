64cc8542a045315c6945fe90928f58e456f18437
Andy <onsubmit@outlook.com>
2022-05-22 10:42:37 -0700

    Updates

---
+++ src/ts/App.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class App {
+    constructor() {
+        this.run = () => {
+            console.log("Hello world!");
+        };
+    }
+}
+exports.default = new App();
+//# sourceMappingURL=App.js.map
